df6cefb7437e302c4307611cc536f7d2424887f7
87takakura <74569038+87takakura@users.noreply.github.com>
2021-02-13 17:45:22 +0900

    ユーザー管理機能実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+ factory :user do
+  #新規登録/ユーザー情報
+  nickname {Faker::Name.initials}
+  email {Faker::Internet.free_email}
+  password {Faker::Internet.password(min_length: 6)}
+  password_confirmation {password}
+  firstname {"山田"}
+  lastname {"太郎"} 
+  katakanafirstname{"ヤマダ"}
+  katakanalastname {"タロウ"}
+  birthday {"1111-11-11"}
+  #新規登録/ユーザー情報
+
+ end
+end
