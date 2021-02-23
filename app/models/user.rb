adfcd661f34caa8c6e95e94909f0d18c62c396e0
87takakura <74569038+87takakura@users.noreply.github.com>
2021-02-23 19:35:38 +0900

    tyousei

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   # validates :email, uniqueness: true
-
   # validates :encrypted_password, uniqueness: true
 
   validates :birthday, presence: true
